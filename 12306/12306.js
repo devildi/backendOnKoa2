06b878038d202c88ccb733f4b1c8572641987d61
DevilDI <DevilDI@appledeMacBook-Pro-3.local>
2018-11-06 13:15:01 +0800

    11-6

--- 12306/12306.js
+++ 12306/12306.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer')
 const url = 'https://kyfw.12306.cn/otn/leftTicket/init'
 let from = 'beijing'
 let to = 'shenyang'
+
 let arrFrom = [...from]
 let arrTo = [...to]
 const sleep = time => new Promise(resolve => {
@@ -30,24 +31,48 @@ const sleep = time => new Promise(resolve => {
 		await page.keyboard.press(arrTo[i]);
 	}
 	await page.keyboard.press('Enter')
-	await page.click('#query_ticket')
-	await sleep(5000)
 
-	//const bodyHandle = await page.$('.ticket-info')
+	await page.click('#date_range>ul>li:nth-child(2)')
+	await sleep(2000)
 
 	const result = await page.evaluate( () => {
-			var result = document.querySelectorAll('.ticket-info > .train')
-
+			var result = []
+			var arr = [...document.querySelectorAll('.ticket-info')]
+			for (let i = 0; i < arr.length; i++){
+				let No = arr[i].querySelector('.train > div > a').innerText
+				let depart = arr[i].querySelector('.cds>.start-t ').innerText
+				let arrive = arr[i].querySelector('.cds>.color999').innerText
+				result.push({No, depart, arrive})
+			}
 			return result
 		}
 	)
-	//await sleep(3000)
-
 	await page.screenshot({path: 'example.png',fullPage: true})
 
 	browser.close()
 
-	console.log(result)
+	let hasGOrD = []
+	let overNight = []
+	result.map((r) => {
+		filter(r, hasGOrD, overNight)
+	})
+	
+	let data = {
+		from,
+		to,
+		hasGOrD,
+		overNight
+	}
+	console.log(data)
 	//process.send({result})
 	//process.exit(0)
 })()
+
+const filter = (obj, hasGOrD, overNight) => {
+	if(obj.No.startsWith('G') || obj.No.startsWith('D')){
+		hasGOrD.push(obj.No)
+	}
+	if(parseInt(obj.depart.slice(0, 2)) >= 17 &&  parseInt(obj.arrive.slice(0, 2))<= 9){
+		overNight.push(obj.No)
+	}
+}
